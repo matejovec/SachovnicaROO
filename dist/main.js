9e3ec649380457d8d2f1e91fbbb9f6c0d4a9b689
Matej Molnar <matej.molnar08@gmail.com>
2026-09-16 10:05:51 +0200

    Funkcna Sachovnica

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,6 @@
-let width = document.getElementById('container')!.clientWidth;
-let height = document.getElementById('container')!.clientHeight;
+"use strict";
+let width = document.getElementById('container').clientWidth;
+let height = document.getElementById('container').clientHeight;
 let boxWidth = width / 8;
 let boxHeight = height / 8;
 for (let i = 0; i < 8; i++) {
@@ -8,6 +9,6 @@ for (let i = 0; i < 8; i++) {
         box.style.width = boxWidth + 'px';
         box.style.height = boxHeight + 'px';
         box.style.backgroundColor = (i + j) % 2 === 0 ? 'white' : 'black';
-        document.getElementById('container')!.appendChild(box);
+        document.getElementById('container').appendChild(box);
     }
 }
